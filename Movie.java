2a4b0dade7f09d20049aa3db817937cbfb2e19d1
naniim <75332203+naniim@users.noreply.github.com>
2022-11-23 19:30:22 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -33,7 +33,12 @@ public class Movie {
         return thisAmount;
    }
   
-  
+    public int getFrequentRenterPoints(int daysRented){
+    //Adicionar o trecho de código extraído.
+    if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
+            daysRented > 1) {return 2;}
+         return 1;
+    }
     public int getPriceCode() {
         return _priceCode;
     }
